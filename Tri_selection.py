fa243208a92ebb441512eae2e3b6ac44f5e75c48
Cfournymanassevitz <carole.fourny@Digital-Grenoble.local>
2024-02-06 16:57:32 +0100

    en cour

---
+++ Tri_selection.py
@@ -0,0 +1,19 @@
+T = [3,5,0,4,5,8,7,2,6,1]
+print(T)
+def swap(idxi,idxj):
+    temp = T[idxi]
+    T[idxi] = T[idxj]
+    T[idxj] = temp
+def tri_selection(T):
+    nb = len(T)
+    for i in range(0,nb):
+        min = i
+        for j in range(i+1,nb):
+            if T[j] < T[min]:
+                min = j
+        swap(T[i],T[min])
+        print(T)
+    return (T)
+
+print(tri_selection(T))
+
